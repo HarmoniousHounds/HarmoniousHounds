3dbddec04abf61e36fe0947ffae93a7aaccf6729
Darren Pearce <darren@pearces.com.au>
2023-11-18 18:50:15 +1100

    Finished adding initial content to home page.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div align="center">
+  <div>
     <b-container>
     <b-row class="justify-content-md-center">
       <b-col md="10">
@@ -12,9 +12,18 @@
             <b-col md="7">
               <b-card-body>
                 <b-card-text>
+                  Hi, my name is Lyn, and my passion is dogs.
+                  I have always had a special interest in animals, and have been involved in training my
+                  own dogs for many years. My specialties include conformation and obedience, agility, retrieval
+                  and scent work.
+                  <br />
+                  <br />
                   I am a qualified dog trainer, having completed Certificate IV in Companion Animal
                   Services. Dogs learn best with positive reinforcement and force free training. Trust and
-                  respect are essential for any living being to learn effectively. Dogs do not speak the
+                  respect are essential for any living being to learn effectively.
+                  <br />
+                  <br />
+                  Dogs do not speak the
                   same language as we do, so if we are to communicate effectively with them, it is
                   essential that we form a relationship built on trust. I do not use training methods
                   which make use of punishment or intimidation.
@@ -29,12 +38,30 @@
         <b-card no-body class="overflow-hidden">
           <b-row no-gutters>
             <b-col md="7">
-              <b-card-body>
+              <b-card-body title="Services">
                 <b-card-text>
-                  Hi, my name is Lyn, and my passion is dogs.
-                  I have always had a special interest in animals, and have been involved in training my
-                  own dogs for many years. Beginning with conformation and obedience, agility, retrieval
-                  and scent work.
+                  <ul>
+                    <li>In home individual puppy training
+                    </li>
+                    <li>In home individual dog training consultations where I can assist with behaviour modification plans
+                    </li>
+                    <li>Teenage to older dog classes, focusing on helping your dog to be a harmonious hound
+                    </li>
+                  </ul>
+
+                  I can help you with:
+                  <ul>
+                    <li>Toilet training your puppy
+                    </li>
+                    <li>Place training
+                    </li>
+                    <li>Alone time
+                    </li>
+                    <li>Socialisation
+                    </li>
+                    <li>and more...
+                    </li>
+                  </ul>
                 </b-card-text>
               </b-card-body>
             </b-col>
@@ -43,44 +70,6 @@
             </b-col>
           </b-row>
         </b-card>
-
-        <b-card no-body class="overflow-hidden">
-          <b-row no-gutters>
-            <b-col md="5">
-              <b-card-img :src="require('../assets/img1.jpg')" alt="Image" class="rounded-0"></b-card-img>
-            </b-col>
-            <b-col md="7">
-              <b-card-body title="Services">
-                <b-card-text>
-                  I am a qualified dog trainer, having completed Certificate IV in Companion Animal
-                  Services. Dogs learn best with positive reinforcement and force free training. Trust and
-                  respect are essential for any living being to learn effectively. Dogs do not speak the
-                  same language as we do, so if we are to communicate effectively with them, it is
-                  essential that we form a relationship built on trust. I do not use training methods
-                  which make use of punishment or intimidation.
-                </b-card-text>
-              </b-card-body>
-            </b-col>
-          </b-row>
-        </b-card>
-
-
-SERVICES
-
-• In home individual puppy training;
-• In home individual dog training consultations where I can assist with behaviour
-modification plans;
-• Teenage to older dog classes, focusing on helping your dog to be a harmonious hound.
-
-I CAN HELP YOU WITH-
-Toilet training your puppy
-
-Leash walking and leash manners
-
-Place training
-Alone time
-Socialisation
-And more...
 </b-col>
     </b-row>
     </b-container>
