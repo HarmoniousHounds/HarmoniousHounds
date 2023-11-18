9c352540b5846d7e3bdead41c17b41e8e591b715
Darren Pearce <darren@pearces.com.au>
2023-11-18 18:39:13 +1100

    Added text from Lyn and photos from Facebook

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,87 @@
 <template>
-  <div>
+  <div align="center">
     <b-container>
-    <b-row class="justify-content-center">
-    See my services here
+    <b-row class="justify-content-md-center">
+      <b-col md="10">
+
+        <b-card no-body class="overflow-hidden">
+          <b-row no-gutters>
+            <b-col md="5">
+              <b-card-img :src="require('../assets/img1.jpg')" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col md="7">
+              <b-card-body>
+                <b-card-text>
+                  I am a qualified dog trainer, having completed Certificate IV in Companion Animal
+                  Services. Dogs learn best with positive reinforcement and force free training. Trust and
+                  respect are essential for any living being to learn effectively. Dogs do not speak the
+                  same language as we do, so if we are to communicate effectively with them, it is
+                  essential that we form a relationship built on trust. I do not use training methods
+                  which make use of punishment or intimidation.
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+        </b-card>
+
+<br />
+
+        <b-card no-body class="overflow-hidden">
+          <b-row no-gutters>
+            <b-col md="7">
+              <b-card-body>
+                <b-card-text>
+                  Hi, my name is Lyn, and my passion is dogs.
+                  I have always had a special interest in animals, and have been involved in training my
+                  own dogs for many years. Beginning with conformation and obedience, agility, retrieval
+                  and scent work.
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+            <b-col md="5">
+              <b-card-img :src="require('../assets/img2.jpg')" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+          </b-row>
+        </b-card>
+
+        <b-card no-body class="overflow-hidden">
+          <b-row no-gutters>
+            <b-col md="5">
+              <b-card-img :src="require('../assets/img1.jpg')" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col md="7">
+              <b-card-body title="Services">
+                <b-card-text>
+                  I am a qualified dog trainer, having completed Certificate IV in Companion Animal
+                  Services. Dogs learn best with positive reinforcement and force free training. Trust and
+                  respect are essential for any living being to learn effectively. Dogs do not speak the
+                  same language as we do, so if we are to communicate effectively with them, it is
+                  essential that we form a relationship built on trust. I do not use training methods
+                  which make use of punishment or intimidation.
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+        </b-card>
+
+
+SERVICES
+
+• In home individual puppy training;
+• In home individual dog training consultations where I can assist with behaviour
+modification plans;
+• Teenage to older dog classes, focusing on helping your dog to be a harmonious hound.
+
+I CAN HELP YOU WITH-
+Toilet training your puppy
+
+Leash walking and leash manners
+
+Place training
+Alone time
+Socialisation
+And more...
+</b-col>
     </b-row>
     </b-container>
   </div>
@@ -17,38 +96,4 @@ export default {
 }
 </script>
 <style>
-
-@media (max-width: 768px) {
-    .b-carousel-slide h3 {
-        font-size: 1.3rem;
-    }
-    .b-carousel-slide p {
-        font-size: 0.8rem;
-    }
-}
-
-.b-carousel-slide {
-    max-height: 54vh;
-}
-.carousel-caption
-			{
-				position: absolute;
-				right: 0;
-				bottom: 0;
-				left: 0;
-				z-index: 10;
-				padding-top: 20px;
-				padding-bottom: 20px;
-				color: #fff;
-				text-align: center;
-				background: rgba(0,0,0,0.3);
-      }
-
-.card-img-top {
-    width: 4rem;
-    height: 4rem;
-    object-fit: cover;
-    text-align: center;
-}
-
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,19 +2,16 @@
     <div>
       <!--  Add padding so that when the scroll bar disappears when moving between pages the navbar doesn't shift.
             The +5vw is so that the menu isn't butted up against the left side of the frame when viewed on a mobile device with no scroll bar. -->
-  <b-navbar toggleable="md" variant="white" style="padding-left: calc(100vw - 100% + 5vw);" fixed="top">
+  <b-navbar variant="white" fixed="top">
     <b-container>
-      <b-navbar-brand :to="{ name: 'Home' }" @click="IconClicked()"><img src="../assets/HHLogo.png" width="100rem"></b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav v-model="navbarCollapseVisible">
-      <b-navbar-nav class="ml-auto" right style="margin-right: 0%;">
+      <b-navbar-nav style="margin-right: 0%;">
         <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
         <b-nav-item :to="{ name: 'Contact' }">Contact</b-nav-item>
       </b-navbar-nav>
-    </b-collapse>
+      <b-navbar-brand :to="{ name: 'Home' }" @click="IconClicked()"><img src="../assets/HHLogo.png" width="100rem"></b-navbar-brand>
     </b-container>
   </b-navbar>
+  <br />
   <br />
   <br />
 </div>
